711df38a7f0c404fc04d3f8f566a58b4033b3e58
Otto Sabart <seberm@seberm.com>
2023-04-08 21:00:00 +0200

    btc: rename module, basic functionality drafts

---
+++ fullnodectl/modules/btc/__init__.py
@@ -0,0 +1,89 @@
+"""
+Module description TODO
+
+# Refs.:
+# - https://github.com/petertodd/python-bitcoinlib/blob/master/bitcoin/rpc.py
+"""
+import logging
+
+from bitcoin import rpc
+
+from fullnodectl import (
+    mod,
+    errors,
+)
+
+__license__ = "MIT"
+log = logging.getLogger(__name__)
+
+MODULE_NAME = "btc"
+MODULE_AUTHOR = "Otto Sabart"
+MODULE_AUTHOR_EMAIL = "seberm@seberm.com"
+MODULE_DESCRIPTION = __doc__
+
+
+def init_parsers(parser):
+    log.debug("Initializing module parser")
+    p_bitcoin = parser.add_subparsers(
+        dest="action",
+        help="Action name",
+        required=True,
+    )
+    p_bitcoin.add_parser("fees", help="Get current transaction fees")
+
+    p_tx = p_bitcoin.add_parser("tx", help="Get information about specific transaction by its TXID")
+    p_tx.add_argument("txid", help="Transaction ID (TXID)")
+
+    p_block = p_bitcoin.add_parser("block", help="Get information about blocks")
+
+    # TODO: Also support block hashes
+    p_block.add_argument(
+        "block_id",
+        help="Block ID (number). Without this argument program will return the information about the last block.",
+        nargs="*",
+    )
+
+
+def main(args, config):
+    log.debug(f"Running module {MODULE_NAME}")
+
+    ACTIONS = {
+        "fees": action_fees,
+        "tx": action_tx,
+        "block": action_block,
+    }
+    act = ACTIONS.get(args.action)
+    act(args, config)
+
+    return 0
+
+
+MODULE_HOOKS = {
+    mod.HOOK_INIT: lambda *args: None,
+    mod.HOOK_INIT_PARSERS: init_parsers,
+    mod.HOOK_RUN: main,
+}
+
+
+def action_fees(args, config):
+    print("Fees are ... TODO")
+
+
+def action_block(args, config):
+    proxy = rpc.Proxy(config["bitcoin"]["url"])
+
+    try:
+        if not args.block_id:
+            log.debug("No block hash/number was provided, getting info about current best-block height.")
+            args.block_id.append(proxy.getblockcount())
+
+        #block_hash = bytes(args.hash[0], "UTF-8")
+        block_hash = proxy.getblockhash(int(args.block_id[0]))
+        print(proxy.getblock(block_hash))
+    except rpc.JSONRPCError as e:
+        log.error(e)
+        raise errors.FullNodeCTLError("There was a problem when connecting to bitcoin RPC.")
+
+
+def action_tx(args, config):
+    print(f"info about tx '{args.txid}' is ... TODO")
